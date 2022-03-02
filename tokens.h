6e5573568ff3ea06fbb098f0800194796dbfec52
Dimitrios Gennimatas <71353114+gennimat@users.noreply.github.com>
2022-03-02 16:38:19 +0200

    Add files via upload
    Part 1 of the project. implementing the lexical analyzer with FLEX

---
+++ tokens.h
@@ -0,0 +1,9 @@
+// Compiler Theory and Design
+// Dr. Duane J. Jarc
+
+// This file contains the enumerated type definition for tokens
+
+enum Tokens {RELOP = 256, ADDOP, MULOP, ANDOP, BEGIN_, BOOLEAN, END, ENDREDUCE,
+	FUNCTION, INTEGER, IS, REDUCE, RETURNS, IDENTIFIER, INT_LITERAL, 
+	ARROW, CASE, ELSE, ENDCASE, ENDIF, IF, OTHERS, REAL, THEN, WHEN, 
+	OROP, NOTOP, REMOP, EXPOP, REAL_LITERAL, BOOL_LITERAL};
